eb6bfa2328a208397a93af9124399bb290ba5c0e
Kiryl Tsvilik <kiryl.t@gmail.com>
2014-07-18 22:18:25 +0300

    created maven project with 3 modules

---
+++ contacts-app/contacts-app-bl/src/main/java/com/itechart/training/tsvilik/contactsapp/bl/App.java
@@ -0,0 +1,13 @@
+package com.itechart.training.tsvilik.contactsapp.bl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ contacts-app/contacts-app-dal/src/main/java/com/itechart/training/tsvilik/contactsapp/dal/App.java
@@ -0,0 +1,13 @@
+package com.itechart.training.tsvilik.contactsapp.dal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
