d4b210d10a8db6614d2b7cdaa86a68079528fda5
Kiryl Tsvilik <kiryl.t@gmail.com>
2014-08-10 15:09:34 +0300

    moved db properties to file

--- contacts-app/contacts-app-dal/src/main/java/com/itechart/training/tsvilik/contactsapp/dal/dao/mysql/MySqlDaoFactory.java
+++ contacts-app/contacts-app-dal/src/main/java/com/itechart/training/tsvilik/contactsapp/dal/dao/mysql/MySqlDaoFactory.java
@@ -1,7 +1,11 @@
 package com.itechart.training.tsvilik.contactsapp.dal.dao.mysql;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
 import org.apache.tomcat.jdbc.pool.DataSource;
-import org.apache.tomcat.jdbc.pool.PoolConfiguration;
 
 import com.itechart.training.tsvilik.contactsapp.dal.dao.AttachmentDao;
 import com.itechart.training.tsvilik.contactsapp.dal.dao.ContactDao;
@@ -13,7 +17,34 @@ import com.itechart.training.tsvilik.contactsapp.dal.dao.PhotoDao;
 import com.itechart.training.tsvilik.contactsapp.dal.dao.RelationshipStatusDao;
 
 public class MySqlDaoFactory implements DaoFactory {
+	private static Logger logger = Logger.getLogger(MySqlDaoFactory.class);
+	
+	private static final String DB_URL;
+	private static final String DB_USER;
+	private static final String DB_PASSWORD;
 
+	static {
+		String filename = "db.properties";
+		ClassLoader classLoader = MySqlDaoFactory.class.getClassLoader();
+		InputStream input = classLoader.getResourceAsStream(filename);
+		Properties properties = new Properties();
+		String dbUrl = null;
+		String dbUser = null;
+		String dbPassword = null;
+		try {
+			properties.load(input);
+			dbUrl = properties.getProperty("url");
+			dbUser = properties.getProperty("user");
+			dbPassword = properties.getProperty("password");
+		} catch (IOException | NumberFormatException e) {
+			logger.error("Failed to get db properties.", e);
+		} finally {
+			DB_URL = dbUrl == null ? "jdbc:mysql://localhost:3306/contacts-app-dev" : dbUrl;
+			DB_USER = dbUser == null ? "root" : dbUser;
+			DB_PASSWORD = dbPassword == null ? "root" : dbPassword;
+		}
+	}
+	
 	private DataSource dataSource;
 
 	private ContactDao contactDao;
@@ -27,13 +58,15 @@ public class MySqlDaoFactory implements DaoFactory {
 	public MySqlDaoFactory() {
 		dataSource = new DataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/contacts-app-dev");
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		dataSource.setUrl(DB_URL);
+		dataSource.setUsername(DB_USER);
+		dataSource.setPassword(DB_PASSWORD);
 		dataSource.setInitialSize(5);
 		dataSource.setMaxActive(10);
 		dataSource.setMaxIdle(5);
 		dataSource.setMinIdle(2);
+		dataSource.setTimeBetweenEvictionRunsMillis(5000);
+		dataSource.setMinEvictableIdleTimeMillis(5000);
 	}
 	
 	@Override
